c26b8b6e7ad0f3d0448d5e185bc096cbd3be8a31
cslabcse <139410998+cslabcse@users.noreply.github.com>
2023-08-01 04:38:04 +0530

    Update FCFS.c

--- verbose/pr5/FCFS.c
+++ verbose/pr5/FCFS.c
@@ -1,18 +1,12 @@
-// FCFS Scheduling algorithm..
-
 #include <stdio.h>
 #include <stdlib.h>
-
 struct process
 {
-    int pid, at, bt, tt, ct, wt;
+    int pid, at, bt, wt, tt, ct;
 };
-
 typedef struct process p;
 
-float tot_tt = 0;
-float tot_wt = 0;
-
+int tot_tt = 0, tot_wt = 0;
 void sort(p *a, int n)
 {
     p temp;
@@ -29,24 +23,23 @@ void sort(p *a, int n)
         }
     }
 }
-
-void printGanttChart(p *p, int n)
+void pgc(p *a, int n)
 {
-    for (int i = 0; i < n; i++)
+    printf("|P%d %d", a[0].pid, a[0].ct);
+    for (int i = 1; i < n; i++)
     {
-        if (p[i].at > p[i - 1].ct)
-            printf("|IDLE (%d) %d", p[i].at, p[i - 1].ct);
-        printf("|%d (%d) %d", p[i].pid, p[i].bt, p[i].ct);
+        if (a[i].at > a[i - 1].ct)
+        {
+            printf("|IDLE %d", a[i].at);
+        }
+        printf("|P%d %d", a[i].pid, a[i].ct);
     }
-    printf("\n");
 }
-
 void CT(p *a, int n)
 {
     int ct;
-    a[0].ct = a[0].at + a[0].bt;
-    ct = a[0].ct;
-
+    ct = a[0].at + a[0].bt;
+    a[0].ct = ct;
     for (int i = 1; i < n; i++)
     {
         if (ct < a[i].at)
@@ -57,7 +50,6 @@ void CT(p *a, int n)
         a[i].ct = ct;
     }
 }
-
 void TT(p *a, int n)
 {
     for (int i = 0; i < n; i++)
@@ -66,7 +58,6 @@ void TT(p *a, int n)
         tot_tt += a[i].tt;
     }
 }
-
 void WT(p *a, int n)
 {
     for (int i = 0; i < n; i++)
@@ -75,8 +66,7 @@ void WT(p *a, int n)
         tot_wt += a[i].wt;
     }
 }
-
-int main()
+void main()
 {
     int n;
     printf("Enter the Number of Process : ");
@@ -92,19 +82,16 @@ int main()
         printf("BT of p%d : ", i + 1);
         scanf("%d", &a[i].bt);
     }
-
     sort(a, n);
     CT(a, n);
     TT(a, n);
     WT(a, n);
-    printGanttChart(a, n);
-
-    printf("PID\tAT\tBT\tWT\tTT\tCT\n");
-
+    pgc(a, n);
+    printf("\nPID\tAT\tBT\tWT\tTT\tCT\n");
     for (int i = 0; i < n; i++)
     {
         printf("%d\t%d\t%d\t%d\t%d\t%d\n", a[i].pid, a[i].at, a[i].bt, a[i].wt, a[i].tt, a[i].ct);
     }
-    printf("\nThe Average Total Time is %f\n", (tot_tt / n));
-    printf("\nThe Average Waiting Time is %f\n", (tot_wt / n));
+    printf("\nThe Average Total Time is %f\n", ((float)tot_tt / n));
+    printf("\nThe Average Waiting Time is %f\n", ((float)tot_wt / n));
 }
